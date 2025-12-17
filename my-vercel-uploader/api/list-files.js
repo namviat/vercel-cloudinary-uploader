2d3ff22bcd37b3bae03f886ba3ab691617252d63
namviat <namviat06@gmail.com>
2025-12-17 18:02:42 +0530

    Update list-files.js

--- my-vercel-uploader/api/list-files.js
+++ my-vercel-uploader/api/list-files.js
@@ -8,7 +8,8 @@ module.exports = async (req, res) => {
   const API_SECRET = process.env.CLOUDINARY_API_SECRET || '0AhRs9vHrqghA5ZcXRyMckXlGjk';
   const UPLOAD_FOLDER = process.env.CLOUDINARY_UPLOAD_FOLDER || 'mycloud';
   const BASE_FOLDER = 'mycloud';
-  const token = req.query.token;
+  
+ const token = req.query.token;
 
   if (!token) {
     return res.status(400).json({ error: 'Missing token' });
@@ -21,11 +22,12 @@ module.exports = async (req, res) => {
   });
 
   try {
-    const result = await cloudinary.search
-      .expression(`folder=${BASE_FOLDER}/${token}`)
-      .sort_by('created_at', 'desc')
-      .max_results(100)
-      .execute();
+    const result = await cloudinary.api.resources({
+      resource_type: 'raw',          //THIS IS THE KEY
+      type: 'upload',
+      prefix: `${BASE_FOLDER}/${token}/`,
+      max_results: 100
+    });
 
     return res.status(200).json({
       token,
@@ -34,6 +36,6 @@ module.exports = async (req, res) => {
 
   } catch (error) {
     console.error('Cloudinary list error:', error);
-    return res.status(500).json({ error: 'Failed to fetch files' });
+    return res.status(500).json({ error: error.message });
   }
 };
