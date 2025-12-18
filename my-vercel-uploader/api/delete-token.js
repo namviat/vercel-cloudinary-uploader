7a2fb38d4a34bebe9e6a6794ae4f9c3baec23790
namviat <namviat06@gmail.com>
2025-12-18 20:11:01 +0530

    Update delete-token.js

--- my-vercel-uploader/api/delete-token.js
+++ my-vercel-uploader/api/delete-token.js
@@ -15,6 +15,11 @@ module.exports = async (req, res) => {
     return res.status(400).json({ error: 'Token required' });
   }
 
+  cloudinary.config({
+   cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dzvz7kzin',
+    api_key: process.env.CLOUDINARY_API_KEY || '484797141727837',
+    api_secret: process.env.CLOUDINARY_API_SECRET || '0AhRs9vHrqghA5ZcXRyMckXlGjk'
+  });
 
   try {
     let nextCursor = null;
